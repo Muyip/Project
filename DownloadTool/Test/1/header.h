9fa4175066a40fde160882d9026c1edd8de1b2b8
common1994 <yp_abc2015@163.com>
2016-11-04 00:57:08 +0800

    使用线程池，增加多线程下载功能; 加入ncurses库，简单实现下载界面; 支持断点续传

--- DownloadTool/Test/1/header.h
+++
@@ -1,9 +0,0 @@
-
-//#include "config.h"
-#include <iostream>
-#include <pthread.h>
-using namespace std;
-
-int Add(int x, int y);
-
-
--- DownloadTool/Test/a.sh
+++
@@ -1,7 +0,0 @@
-
-count = 0
-while ( count < 100 )
-do
-	cat file >> file
-	++count;
-done
